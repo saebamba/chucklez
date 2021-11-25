ac108b2afa2cbfa5bf1843e9e3d0fdc8b01321af
Sae Bamba <saesp97@gmail.com>
2021-11-24 23:21:59 -0500

    123zxc

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="home">
     
-    <br>
-    <br>
     <HelloWorld msg="DISTRICT CHUCKLES" />
     <img alt="Vue logo" src="../assets/logo.png" width="300" height="200">
     <br>
-
+<br>
     <h3>Laughter is a healing medicine! <br>Good vibes on demand, brighten your day as easy as ordering an Uber :)</h3>
 
 <br>
@@ -18,10 +16,7 @@
 />
 </div>
 <br>
-    <br>
-   
-    <br>
-    <br>
+
      <div v-if="!$auth.loading">
       <!-- show login when not authenticated -->
       <button class="btn default" v-if="!$auth.isAuthenticated" @click="login">Order your first joke!</button>
