ffa161b3fa0993147f3b16970f709267af5eb62b
Sae Bamba <saesp97@gmail.com>
2021-12-23 15:26:31 -0500

    done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,11 @@
     
 
 <br>
-<br>
+
 <img src="@/assets/logo23.png" class="image-fit" width="380" height="150">
 <br>
 <br>
-
+<br>
 <br>
 <h3>Start chuckling.<br>As easy as calling a Lyft!</h3>
      <br>
